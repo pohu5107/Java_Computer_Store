51c291df565a27fd08997a6038dcf3249b32ec4b
pohu5107 <phu123574896@gmail.com>
2026-03-06 15:13:57 +0700

    .

--- src/main/java/DAO/brandsDAO.java
+++ src/main/java/DAO/brandsDAO.java
@@ -81,17 +81,5 @@ public class BrandsDAO {
         }
     }
     
-    public boolean updateStock(String productId, int amount) {
-        // amount > 0 là nhập thêm, amount < 0 là bán đi
-        String sql = "UPDATE Products SET Quantity = Quantity + ? WHERE ProductID = ?";
-        try (Connection conn = connectDB.getConnection();
-             PreparedStatement pst = conn.prepareStatement(sql)) {
-            pst.setInt(1, amount);
-            pst.setString(2, productId);
-            return pst.executeUpdate() > 0;
-        } catch (SQLException e) {
-            e.printStackTrace();
-            return false;
-        }
-    }
+
 }
